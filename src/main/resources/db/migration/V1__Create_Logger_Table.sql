e61cf935181fe0ca57cb8360bb684937308dda71
pablogore <pablo.m.gore@gmail.com>
2018-10-26 20:17:59 -0300

    add docker for mysql & configuration for flyway migration using gradle plugin

--- src/main/resources/db/migration/V1__Create_Logger_Table.sql
+++ src/main/resources/db/migration/V1__Create_Logger_Table.sql
@@ -1,8 +1,8 @@
 create table access_logger(
     ID varchar(50) not null PRIMARY KEY,
-    DATE DATE not null,
+    D DATE not null,
     IP varchar(50) not null,
     REQUEST varchar(50) not null,
     STATUS varchar(100) not null,
-    USER_AGENT VARCHAR not null
+    USER_AGENT text not null
 );
